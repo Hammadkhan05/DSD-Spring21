85d5688114b3dee685bb58232514838b9b62094a
hammad-ahmed-98 <79632389+hammad-ahmed-98@users.noreply.github.com>
2021-02-25 17:32:17 +0500

    Add files via upload

---
+++ lab-01/Full_adder.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/25/2021 03:21:57 PM
+// Design Name: 
+// Module Name: Full_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Full_adder(sum, c_out, a, b, c_in);
+input wire a, b, c_in;
+output wire sum, c_out;
+
+// wires
+wire s1, c1, s2;
+
+// logic description
+
+xor xor1 (s1, a ,b);
+xor xor2(sum, s1, c_in);
+
+and and1(c1, a, b);
+and and2(s2, s1, c_in);
+
+xor xor3(c_out, c1, s2);
+
+endmodule
